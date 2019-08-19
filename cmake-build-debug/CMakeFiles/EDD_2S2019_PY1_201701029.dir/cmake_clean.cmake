672377844d3a6d4dafb43e85b9388bcc71bd0e2d
EdsonArmando <guixarmando@gmail.com>
2019-08-19 13:38:15 -0700

    First Commit

---
+++ cmake-build-debug/CMakeFiles/EDD_2S2019_PY1_201701029.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EDD_2S2019_PY1_201701029.dir/main.cpp.obj"
+  "EDD_2S2019_PY1_201701029.pdb"
+  "EDD_2S2019_PY1_201701029.exe"
+  "EDD_2S2019_PY1_201701029.exe.manifest"
+  "EDD_2S2019_PY1_201701029.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EDD_2S2019_PY1_201701029.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/EDD_2S2019_PY1_201701029.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for EDD_2S2019_PY1_201701029.
+# This may be replaced when dependencies are built.
